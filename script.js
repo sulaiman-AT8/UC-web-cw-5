1d67aaa5dd959aba5b7194bb39501ce522a1d7cd
sulaiman-AT8 <142622155+sulaiman-AT8@users.noreply.github.com>
2023-09-04 19:58:38 +0300

    done

---
+++ script.js
@@ -0,0 +1,30 @@
+alert ("مرحباً بك في موقعي أنا سليمان وسوف أحسب درجتك")
+let grade = prompt ("أدخل درجتك")
+console.log(grade
+    )
+
+    if (grade >= 90 && grade <= 100) {
+      console.log("%cلقد حصلت على امتياز", " color: blue; background-color: white")  
+    }
+
+    else if (grade >= 80 && grade <= 89 ) {
+        console.log("لقد حصلت على جيد جداً")  
+      }
+
+     else if (grade >= 70 && grade <= 79 ) {
+        console.log("لقد حصلت على جيد")  
+      }
+
+     else if (grade >= 60 && grade <= 69 ) {
+        console.log("لقد حصلت على مقبول")  
+      }
+      
+      else {
+        console.log("لقد حصلت على راسب")  
+      }
+
+      console.log("جافاسكربت ليست جافا");
+    //   جافاسكربت ليست جافا
+
+    
+
